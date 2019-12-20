1b30db6c95121bfea53152e5a5bfcc35fbb312ab
elazar <a@a.a>
2019-12-20 03:31:05 +0200

    some changes

--- app.js
+++ app.js
@@ -9,31 +9,38 @@ let players = [];
 let obstaclesInterval;
 
 io.on('connection', socket => {
-    console.log('a user connected');
-    console.log('socket id', socket.id);
+    console.log('a user connected with id ', socket.id);
 
-    socket.on('obsticales', (id, obsticaleRival) => {
-        io.to(players.filter(() => playerID !== id)).emit(obsticaleRival);
+    socket.on('obstacles', (obsticaleRival) => {
+        io.to(players.filter((playerID) => playerID !== socket.id))
+            .emit('rival send obstacle', obsticaleRival);
     })
 
-    if (players.length <= 2) {
+    if (players.length < 2) {
         players.push(socket.id);
+
+        if (players.length == 1) {
+            console.log('waiting to second player...');
+        } else if (players.length == 2) {
+            socket.emit('start game', { players: players });
+
+            obstaclesInterval = setInterval(() => {
+                io.to(players[0]).emit('obstacle', { obstacle: obstacle.CROW });
+                io.to(players[1]).emit('obstacle', { obstacle: obstacle.ROCK });
+                io.sockets.emit('obstacle', { obstacle: obstacle.ROCK });
+            }, 3000);
+
+        } else {
+            console.log('something wrong...');
+        }
     }
 
-    if (players.length == 1) {
-        console.log('waiting to second player...');
-    } else if (players.length == 2) {
-        socket.emit('start game', { players: players });
-
-        obstaclesInterval = setInterval(() => {
-            io.to(players[0]).emit('obstacle', { obstacle: obstacle.CROW });
-            io.to(players[1]).emit('obstacle', { obstacle: obstacle.ROCK });
-            io.sockets.emit('obstacle', { obstacle: obstacle.ROCK });
-        }, 3000);
-
-    } else {
-        console.log('something wrong...');
-    }
+    socket.on('game start', data => {
+        console.log(data.room);
+        socket.broadcast
+            .to(data.room)
+            .emit('receive message', data)
+    });
 
     socket.on('disconnect', reason => {
         console.log('user disconnected', socket.id);
@@ -45,13 +52,6 @@ io.on('connection', socket => {
             players.splice(index, 1);
         }
         console.log('there is ' + (players.length + 1) + ' players now.');
-    });
-
-    socket.on('game start', data => {
-        console.log(data.room);
-        socket.broadcast
-            .to(data.room)
-            .emit('receive message', data)
     });
 });
 
